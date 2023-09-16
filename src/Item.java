5fcd25027535109497ad9b4f99f71cb8595084d1
windee <113081325+itswindee@users.noreply.github.com>
2023-09-16 00:02:06 -0400

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Item {
+class Item {
+    double length;
+    double height;
+    double width;
+    double weight;
+
+    public Item(double length, double height, double width, double weight) {
+        this.length = length;
+        this.height = height;
+        this.width = width;
+        this.weight = weight;
+    }
 }
